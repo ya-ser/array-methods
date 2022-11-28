96efdb0bee632eb388ee5fc88fd7c6a3177025e8
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-11-28 10:52:06 -0500

    solution for mapping user obj to new obj

---
+++ mapToObj.js
@@ -0,0 +1,49 @@
+/* Map to objects
+importance: 5
+You have an array of user objects, each one has name, surname and id.
+
+Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+For instance:
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = /* ... your code ... */
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
+*/
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+// make sure to wrap {} in () first as curlies in js mean the start of a func expression not the creation of an obj
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
