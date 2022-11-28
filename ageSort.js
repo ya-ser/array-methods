c4458f52e7083e9ba831924323036506611ee129
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-11-28 11:13:15 -0500

    sorting array of objs by age

---
+++ ageSort.js
@@ -0,0 +1,34 @@
+/* Sort users by age
+importance: 5
+Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+
+For instance:
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+sortByAge(arr);
+
+// now: [john, mary, pete]
+alert(arr[0].name); // John
+alert(arr[1].name); // Mary
+alert(arr[2].name); // Pete
+*/
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+const sortByAge = arr => arr.sort((a, b) => a.age - b.age);
+
+console.log(sortByAge(arr));
+
+// now: [john, mary, pete]
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
